de66ca6b7f51638b83f00f44e8ef0940d0f7e75b
Nathaniel Ketema <nathanielketema@gmail.com>
2025-12-01 00:09:27 -0600

    feat: simplify build script

--- build.zig
+++ build.zig
@@ -4,10 +4,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("advent_of_code_zig", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
+    const test_exe = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
+    const run_cmd = b.addRunArtifact(test_exe);
 
+    const test_step = b.step("test", "Test all solutions");
+    test_step.dependOn(&run_cmd.step);
 }
